c0c09994f72110ee8e6d9c1e286d4bc9c61b9c32
Marco Antonio <ETAURE\marco.araujo@etaure115.etaure.srv>
2021-08-29 16:21:25 -0300

    estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import {FiChevronRight} from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-import { Title } from "./styles";
-const Dashboard:React.FunctionComponent = () => {
-    return <Title>Explore repositórios no Github</Title>
+import { Title, Form , Repositories} from "./styles";
+const Dashboard: React.FunctionComponent = () => {
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+
+            <Form >
+                < input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/59663846?v=4" alt="Marco Antonio" />
+                    <div>
+                    <strong>Project Players</strong>
+                    <p>Projeto de crud de jogadores</p>
+                </div>
+
+                <FiChevronRight  size={20}/>
+                </a>
+
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/59663846?v=4" alt="Marco Antonio" />
+                    <div>
+                    <strong>Project Players</strong>
+                    <p>Projeto de crud de jogadores</p>
+                </div>
+
+                <FiChevronRight  size={20}/>
+                </a>
+
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/59663846?v=4" alt="Marco Antonio" />
+                    <div>
+                    <strong>Project Players</strong>
+                    <p>Projeto de crud de jogadores</p>
+                </div>
+
+                <FiChevronRight  size={20}/>
+                </a>
+              
+            </Repositories>
+        </>
+    )
 }
 export default Dashboard
