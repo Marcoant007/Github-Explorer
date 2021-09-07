d5e749575d435d60b01e7210380b656e541a8507
Marcoant007 <marcoantnovo@gmail.com>
2021-09-06 23:46:01 -0300

    Ultilizando UseEffect para salvar os cadastros no storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
-import React, {useState, FormEvent} from "react";
-import {FiChevronRight} from 'react-icons/fi';
+import React, { useState, FormEvent, useEffect } from "react";
+import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form , Repositories, Error} from "./styles"; 
+import { Title, Form, Repositories, Error } from "./styles";
 // Aqui é onde eu  importo os componentes que eu criei la no styles
 
 import Repository from "../Repository";
@@ -21,70 +21,83 @@ interface Repository {
 
 
 const Dashboard: React.FunctionComponent = () => {
-    const [newRepo, setNewRepo ] = useState('');
+    const [newRepo, setNewRepo] = useState('');
 
     const [inputError, setInputError] = useState('');
 
-    const [respositories, setRepositories] = useState<Repository[]>([]); //use state é ultilizado para armazena variaveis
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
 
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories); //JSON.parse para desconverter
+        } else {
+            return []
+        }
 
-    async function handeAddRepository(event:FormEvent<HTMLFormElement>) {
+    }); 
+
+   
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
+
+    async function handeAddRepository(event: FormEvent<HTMLFormElement>) {
 
         event.preventDefault();
 
-        if(!newRepo){
+        if (!newRepo) {
             setInputError('Digite o autor/nome do repositório');
             return;
         }
 
-       try {
-            
-        const response = await api.get<Repository>(`repos/${newRepo}`);
-        
-        const respository = response.data
+        try {
 
-        setRepositories([...respositories,respository ]); //preciso pegar oque eu ja tenho e passar o novo por isso os '...'
-        setInputError('');
-        setNewRepo('');
-       } catch (error) {
-        setInputError('Erro na busca de repositório')
-       }
-     }
-   
-    
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data
+
+            setRepositories([...repositories, repository]); //preciso pegar oque eu ja tenho e passar o novo por isso os '...'
+            setInputError('');
+            setNewRepo('');
+        } catch (error) {
+            setInputError('Erro na busca de repositório')
+        }
+    }
+
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-      
+
             <Form hasError={!!inputError} onSubmit={handeAddRepository}>
                 < input
-                value={newRepo}
-                onChange = {(e) => setNewRepo(e.target.value)}
-                placeholder="Digite o nome do repositório" />
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
 
-            {inputError && <Error>{inputError}</Error>}    
+            {inputError && <Error>{inputError}</Error>}
             {/* Isso é um IF que só vai executar quando a primeira parte da condição for verdadeira */}
 
 
             <Repositories>
-              {respositories.map(repository => (
-                    <a key={repository.full_name} href="teste"> 
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
 
-                    <img src={repository.owner.avatar_url} 
-                         alt={repository.owner.login} />
-                    <div>
-                    <strong>{repository.full_name}</strong>
-                    <p>{repository.description}</p>
-                </div>
+                        <img src={repository.owner.avatar_url}
+                            alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
 
-                <FiChevronRight  size={20}/>
-                </a>
+                        <FiChevronRight size={20} />
+                    </a>
 
-              ))}
+                ))}
             </Repositories>
         </>
     )
